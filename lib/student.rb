81d6a3f0862b36235978a4b65296b6e438fa0c38
Angus Morrison <contact@angus-morrison.com>
2019-09-15 11:02:27 +0100

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -13,29 +13,31 @@ class Student
     @id = id
   end
 
-  def save()
-    if self.id()
-      self.update()
+  def save
+    if self.id
+      update
     else
       sql = <<-SQL
         INSERT INTO students (name, grade)
         VALUES (?, ?);
       SQL
-
       DB[:conn].execute(sql, self.name, self.grade)
-      @id = DB[:conn].execute("SELECT last_insert_rowid() FROM students")[0][0]
+
+      sql = "SELECT last_insert_rowid() FROM students LIMIT 1"
+      @id = DB[:conn].execute(sql)[0][0]
     end
   end
 
-  def update()
+  def update
     sql = <<-SQL
       UPDATE students
-      SET name = ?, grade = ?
+      SET name = ?, GRADE = ?
       WHERE id = ?;
     SQL
-
     DB[:conn].execute(sql, self.name, self.grade, self.id)
   end
+
+ 
 
   ###### Class methods ######
 
@@ -66,17 +68,15 @@ class Student
   end
 
   def self.new_from_db(row)
-    new_student = Student.new(row[1], row[2], row[0])
-    new_student
+    Student.new(row[1], row[2], row[0])
   end
 
   def self.find_by_name(name)
     sql = <<-SQL
       SELECT * FROM students
       WHERE name = ?
-      LIMIT 1
+      LIMIT 1;
     SQL
-
     new_from_db(DB[:conn].execute(sql, name)[0])
   end
 
